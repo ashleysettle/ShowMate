1c2d1ad3c8070e934af4491b2dcb5eba23878c00
Ashley Settle <ashley@peregrine.land>
2024-10-21 13:02:22 -0500

    added Firebase and code for log in and create account

--- ShowMate/CreateAccountVC.swift
+++ ShowMate/CreateAccountVC.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CreateAccountVC: UIViewController {
     
     var delegate: UIViewController!
     var createSegueIdentifier = "CreateAccountSegue"
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var confirmPasswordText: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // for my sanity
         // Do any additional setup after loading the view.
     }
     
@@ -25,4 +30,20 @@ class CreateAccountVC: UIViewController {
         }
     }
 
+    @IBAction func createAccountPressed(_ sender: Any) {
+        // need to add username to createUser
+        if(passwordText.text != confirmPasswordText.text){
+            errorMessage.text = "Passwords do not match!"
+            return
+        }
+        // create a new user
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordText.text!) {
+                (authResult,error) in
+                if let error = error as NSError? {
+                    self.errorMessage.text = "\(error.localizedDescription)"
+                }else {
+                    self.errorMessage.text = ""
+                }
+            }
+    }
 }
--- ShowMate/ViewController.swift
+++ ShowMate/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
     
     let createAccountSegueIdentifier = "CreateAccountSegue"
     let signInSegueIdentifier = "SignInSegue"
@@ -30,5 +32,16 @@ class ViewController: UIViewController {
             nextVC.delegate = self
         }
     }
+    @IBAction func signInPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!){
+                    (authResult,error) in
+                    if let error = error as NSError? {
+                        self.errorMessage.text = "\(error.localizedDescription)"
+                    }else {
+                        self.errorMessage.text = ""
+                    }
+                }
+    }
+    
 }
 
